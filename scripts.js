b275f7f57f415573634ec04880db6c665c60ecbc
Davi Maximo Quoos <davimaximoquooss@gmail.com>
2026-01-17 12:20:29 -0300

    menu hamburguer

--- scripts.js
+++ scripts.js
@@ -17,3 +17,18 @@ const observer = new IntersectionObserver(
 
 const reveal = document.querySelectorAll(".reveal");
 reveal.forEach((el) => observer.observe(el));
+
+const hamburger = document.querySelector(".hamburger");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  mobileMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".mobile-menu a").forEach(link => {
+  link.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    mobileMenu.classList.remove("active");
+  });
+});
